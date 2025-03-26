9e9b8f6c69523415a7d9438f022c0723969ab180
anaik91 <ashwinknaik@google.com>
2025-03-26 18:35:51 -0500

    feat: updated workflow

--- scripts/prepare_bundle.py
+++ scripts/prepare_bundle.py
@@ -522,9 +522,9 @@ def main():
             )
 
             api1.validate_proxy(api_name, f"{api_name}.zip")
-            sys.exit(1)
         else:
             logging.error("Bundle creation failed, cannot unzip.")
+            sys.exit(1)
     else:
         logging.error("Bundle creation failed.")
         sys.exit(1)
